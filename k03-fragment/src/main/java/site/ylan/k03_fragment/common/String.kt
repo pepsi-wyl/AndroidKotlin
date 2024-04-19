6f280fa65912bf3b8b824e8a500f9752ac37936d
ylan <pepsiwyl@gmail.com>
2024-04-19 17:44:43 +0800

    1.运算符重载+扩展函数 优化代码

---
+++ k03-fragment/src/main/java/site/ylan/k03_fragment/common/String.kt
@@ -0,0 +1,3 @@
+package site.ylan.k03_fragment.common
+
+operator fun String.times(number: Int): String = repeat(number)
--- k03-fragment/src/main/java/site/ylan/k03_fragment/utils/RandomUtils.kt
+++ k03-fragment/src/main/java/site/ylan/k03_fragment/utils/RandomUtils.kt
@@ -1,11 +1,14 @@
 package site.ylan.k03_fragment.utils
 
+import site.ylan.k03_fragment.common.times
+
 object RandomUtils {
-    fun getRandomLengthString(str: String): String {
-        return StringBuilder().apply {
-            repeat((1..20).random()) {
-                append(str)
-            }
-        }.toString()
-    }
+//    fun getRandomLengthString(str: String): String {
+//        return StringBuilder().apply {
+//            repeat((1..20).random()) {
+//                append(str)
+//            }
+//        }.toString()
+//    }
+    fun getRandomLengthString(str: String): String =  str * (1..20).random()
 }
