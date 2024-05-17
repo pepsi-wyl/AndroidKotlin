76f89ec9216b98421be36d81a5fe941e7b0f764e
ylan <pepsiwyl@gmail.com>
2024-05-17 13:44:16 +0800

    1.viewBinding开启

--- app/k20-constraintlayout/src/main/java/site/ylan/k20_constraintlayout/MainActivity.kt
+++ app/k20-constraintlayout/src/main/java/site/ylan/k20_constraintlayout/MainActivity.kt
@@ -2,10 +2,20 @@ package site.ylan.k20_constraintlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import site.ylan.k20_constraintlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(
+            layoutInflater,
+            null,
+            false
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
