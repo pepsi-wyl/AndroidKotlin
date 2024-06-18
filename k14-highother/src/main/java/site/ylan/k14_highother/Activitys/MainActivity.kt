326244dec2cd1b92df3ae8c30c704da2b3730ca8
ylan <pepsiwyl@gmail.com>
2024-06-18 09:49:50 +0800

    1.highOther-LogUtil

--- k14-highother/src/main/java/site/ylan/k14_highother/Activitys/MainActivity.kt
+++ k14-highother/src/main/java/site/ylan/k14_highother/Activitys/MainActivity.kt
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import site.ylan.k14_highother.Application.MainApplication
 import site.ylan.k14_highother.databinding.ActivityMainBinding
+import site.ylan.k14_highother.utils.LogUtil
 
 class MainActivity : AppCompatActivity() {
+
+    // TAG
+    private val TAG = javaClass.simpleName
 
     // ActivityMainBinding
     private val binding: ActivityMainBinding by lazy {
@@ -19,6 +23,7 @@ class MainActivity : AppCompatActivity() {
 
         // Intent传递对象 测试
         binding.intentTraObject.setOnClickListener {
+            LogUtil.v(TAG, "点击了Intent传递对象")
             startActivity(Intent(MainApplication.globeContext, FirstActivity::class.java))
         }
     }
